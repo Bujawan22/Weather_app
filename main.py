63749985df7e709b619f8c9c62647eed49724a31
Bujawan22 <tarabujawan222403@gmail.com>
2025-08-21 03:21:28 +0800

    weather 1 add

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import requests
+import datetime
+
+app = Flask(__name__, template_folder='frontend')
+
+API_KEY = "ca77bd08362e957a069864c6068e71fb"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+def getWeather(city):
+    url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric&lang=id"
+    response = requests.get(url)
+    return response.json()
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    weather_data = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        data = getWeather(city)
+
+        if data["cod"] == 200:
+            weather_data = {
+                "nama_kota": data["name"],
+                "icon": data["weather"][0]["icon"],
+                "tanggal": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                "suhu": data["main"]["temp"],
+                "kondisi": data["weather"][0]["description"],
+                "kelembapan": data["main"]["humidity"]
+            }
+        else:
+            weather_data = {"error": "Kota tidak ditemukan!"}
+
+    return render_template("index.html", weather=weather_data)
+
+@app.route("/weather_by_coord")
+def weather_by_coord():
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+
+    url = f"{BASE_URL}?lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=id"
+    data = requests.get(url).json()
+
+    if data["cod"] == 200:
+        weather_data = {
+            "nama_kota": data["name"],
+            "icon": data["weather"][0]["icon"],
+            "tanggal": datetime.datetime.now().strftime("%d-%B-%Y %H:%M:%S"),
+            "suhu": round(data["main"]["temp"]),
+            "kondisi": data["weather"][0]["description"],
+            "kelembapan": data["main"]["humidity"]
+        }
+        return weather_data
+    else:
+        return {"error": "Lokasi tidak ditemukan"}
+    
+if __name__ == "__main__":
+    app.run(debug=True)
